083c4e82ba0d12e8b1821bb9e456a70b4432cc1e
kaa08 <48540719+kaa08@users.noreply.github.com>
2025-09-17 10:23:46 +0900

    Delete src directory

--- src/domain/audio/MusicGenre.java
+++
@@ -1,5 +0,0 @@
-package domain.audio;
-
-public enum MusicGenre {
-    POP, ROCK, HIPHOP, JAZZ, SOUL, OST, FORK, COUNTRY, ETC
-}
--- src/domain/video/VideoGenre.java
+++
@@ -1,5 +0,0 @@
-package domain.video;
-
-public enum VideoGenre {
-    ACTION, SF, THRILLER, ROMANCE, ADVENTURE, DRAMA, HORROR, FAMILY, ANIMATION, ETC
-}
